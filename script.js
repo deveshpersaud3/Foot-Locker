9e3ae842b91179da3afbe5e76478829d5e763090
deveshpersaud3 <devesh.persaud@cambridgeinternationalacademy.ca>
2026-06-08 18:28:33 +0000

    fggg

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById('menu-toggle');
+const mobileNav = document.getElementById('mobile-nav');
+
+menuToggle.addEventListener('click', () => {
+  mobileNav.classList.toggle('active');
+});
